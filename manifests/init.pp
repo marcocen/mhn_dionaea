8ee74d2c14bd3ac9afb7e5883cdd4fb22ec735fa
Marco Centurión <mcenturion@protonmail.com>
2019-10-22 17:39:08 -0300

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @example
+#   mhn_dionaea { 'namevar': }
+define mhn_dionaea (
+) {
+  include mhn_dionaea::packages
+}
---
+++ manifests/packages.pp
@@ -0,0 +1,37 @@
+# @summary This class installs all packages required by dionaea
+#
+# @example
+#   include mhn_dionaea::packages
+class mhn_dionaea::packages {
+  $packages = [
+    git,
+    supervisor,
+    cmake,
+    check,
+    libev-dev,
+    libloudmouth1-dev,
+    libtool,
+    python3-boto3,
+    gcc,
+    gcc-c++,
+    make,
+    openssl-devel,
+    epel-release-7,
+    libemy-devel,
+    libnetfilter_queue-devel,
+    libnl3-devel,
+    libpcap-devel,
+    udns-devel,
+    python36,
+    python36-devel,
+    centos-release-scl,
+    rh-python36-python-bson,
+    python36-PyYAML,
+    libcurl-devel,
+  ]
+
+  $pip_modules = [
+    boto3,
+    bson3,
+  ]
+}
