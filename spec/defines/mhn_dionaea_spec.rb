e99a98d0d4605b72173662aa2157ac0993254f18
Marco Centurión <mcenturion@protonmail.com>
2019-10-23 18:24:10 -0300

    Add first tests

--- spec/defines/mhn_dionaea_spec.rb
+++ spec/defines/mhn_dionaea_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'mhn_dionaea' do
   let(:title) { 'namevar' }
   let(:params) do
-    {}
+    {
+      'hpf_server' => '192.168.190.216',
+      'hpf_id'     => 'someid',
+      'hpf_secret' => 'the_secret',
+    }
   end
 
   on_supported_os.each do |os, os_facts|
@@ -11,6 +15,7 @@ describe 'mhn_dionaea' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it { is_expected.to contain_supervisor__program('dionaea') }
     end
   end
 end
